804555ef079250a6c4b85204a4ebbfcb5aa4a430
summved <summved_jain@mckinsey.com>
2020-07-21 15:28:29 +0530

    Update index.js
    changes keys to key

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ var access_key = "sdfsdf";
 var AWS_SECRET_ACCESS_KEY = "TestKeyPassPwd";
 var DSA_PRIVATE_KEY = "sdkflasaskdjfhjksdhfkdsjf";
 var git_token = "3cb43625bb5a9a7f58498517asdsadas7sa7d687a5sd78sa6d";
-var gpg_keys = "8sa7d8sa7d0sa7dsa7d98sad7sa6d7sa6d98sads8a7d0sad7sa87d89sa7d98sa7d98sa7d8sad";
-var keys = "4dsf245dsf896ds8f59ds65f4fdf587F5ds8dsf95ds87f";
-var ssh_keys = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MWRjMA=="
+var gpg_key = "8sa7d8sa7d0sa7dsa7d98sad7sa6d7sa6d98sads8a7d0sad7sa87d89sa7d98sa7d98sa7d8sad";
+var key = "4dsf245dsf896ds8f59ds65f4fdf587F5ds8dsf95ds87f";
+var ssh_key = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MWRjMA=="
