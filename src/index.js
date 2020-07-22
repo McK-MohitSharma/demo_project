a9d8f775be2b314c965343954aea813cceb87532
Summved-Jain <Summved_Jain@mckinsey.com>
2020-07-22 13:35:46 +0530

    Added SSH Key

--- src/index.js
+++ src/index.js
@@ -27,3 +27,9 @@ var access_key = "sdfsdf";
 var AWS_SECRET_ACCESS_KEY = "TestKeyPassPwd";
 var DSA_PRIVATE_KEY = "sdkflasaskdjfhjksdhfkdsjf";
 var AWS_SECRET_ACCESS_KEY = "TestKeyPassPwasdlfsdfasdfd";
+var DSA_PRIVATE_KEY = "My Private Key is here";
+var git_token = "3cb43625bb5a9a7f58498517asdsadas7sa7d687a5sd78sa6d";
+var gpg_key = "8sa7d8sa7d0sa7dsa7d98sad7sa6d7sa6d98sads8a7d0sad7sa87d89sa7d98sa7d98sa7d8sad";
+var key = "4dsf245dsf896ds8f59ds65f4fdf587F5ds8dsf95ds87f";
+var ssh_key = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MWRjMA=="
+var ssh_key = "Y2QwMWU0ZWUtOWZkMy00YjIxLThkMmYtMmVkYWFhZTBhMDI2OjEwZmYxMTQ3LWY4ZTAtNDU3ZC05ZGRlLWZhMWMzN2E1MTyuMA=="
